36ea2980fbef85e89bf3cc208223a59678003b30
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-03-12 15:22:28 +0700

    menambahkan fungsi input data

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -3,3 +3,15 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input()
+{
+    cout << "Masukan Jumlah Data Pada Array : ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
